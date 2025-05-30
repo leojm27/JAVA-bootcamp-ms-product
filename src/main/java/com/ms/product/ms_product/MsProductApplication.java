5c106de9f53e342631f1f1b450b9c8288e77e4bf
Leonardo J Morales <leom6313@gmail.com>
2025-05-29 22:20:11 -0300

    first commit

---
+++ src/main/java/com/ms/product/ms_product/MsProductApplication.java
@@ -0,0 +1,15 @@
+package com.ms.product.ms_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // Exclude DataSourceAutoConfiguration if not using a database
+public class MsProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductApplication.class, args);
+		System.out.println("Product Service is running...");
+	}
+
+}
